bcbceed18b0055c61a06077cce96712c9e4bf62f
Thy <06thy40@gmail.com>
2022-04-11 14:17:24 +0300

    feat : javascript variables

---
+++ 01-variables/script.js
@@ -0,0 +1,10 @@
+// var
+//var serverName = "api.kodluyoruz.org";
+//console.log(serverName);
+
+/************************* */
+
+//let
+let serverName;
+serverName = "https://kodluyoruz.org";
+console.log(serverName);
